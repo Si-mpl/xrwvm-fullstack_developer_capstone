f59d65913db45f37b276b9c34a217185e8918672
Si_mpl <5trace.ed@gmail.com>
2026-01-19 02:22:36 -0500

    part 2 done

--- server/database/app.js
+++ server/database/app.js
@@ -62,7 +62,6 @@ app.get('/fetchReviews/dealer/:id', async (req, res) => {
 app.get('/fetchDealers', async (req, res) => {
     try {
         const documents = await Dealerships.find();
-        console.log('fetched')
         res.json(documents);
     } catch (error) {
         res.status(500).json({ error: 'Error fetching documents' });
@@ -92,7 +91,6 @@ app.get('/fetchDealer/:id', async (req, res) => {
 //Express route to insert review
 app.post('/insert_review', express.raw({ type: '*/*' }), async (req, res) => {
   data = JSON.parse(req.body);
-  console.log("caught")
   const documents = await Reviews.find().sort( { id: -1 } );
   let new_id = documents[0].id+1;
 
@@ -110,7 +108,6 @@ app.post('/insert_review', express.raw({ type: '*/*' }), async (req, res) => {
 
   try {
     const savedReview = await review.save();
-    console.log("saved")
     res.json(savedReview);
   } catch (error) {
 		console.log(error);
--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -10,6 +10,9 @@ backend_url = os.getenv(
 sentiment_analyzer_url = os.getenv(
     'sentiment_analyzer_url',
     default="http://localhost:5050/")
+searchcars_url = os.getenv(
+    'searchcars_url',
+    default="http://localhost:3050/")
 
 
 def get_request(endpoint, **kwargs):
@@ -32,6 +35,27 @@ def get_request(endpoint, **kwargs):
         print("Network exception occurred on get request")
 
 
+def searchcars_request(endpoint, **kwargs):
+    params = ""
+    if (kwargs):
+        for key, value in kwargs.items():
+            params = params + key + "=" + value + "&"
+
+    request_url = backend_url + endpoint + "?" + params
+
+    print("GET from {} ".format(request_url))
+    try:
+        # Call get method of requests library with URL and parameters
+        response = requests.get(request_url)
+        return response.json()
+    except Exception as err:
+        # If any error occurs
+        print(f"Unexpected {err=}, {type(err)=}")
+        print("Network exception occurred on get request")
+    finally:
+        print("GET request call complete!")
+
+
 def analyze_review_sentiments(text):
     request_url = sentiment_analyzer_url+"analyze/"+text
     try:
@@ -52,3 +76,6 @@ def post_review(data_dict):
     except Exception as err:
         print(f"Unexpected {err=}, {type(err)=}")
         print("Network exception occurred")
+
+
+
